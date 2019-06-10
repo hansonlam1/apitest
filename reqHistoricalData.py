715ffa258a8e2e8f9adc1b0a3e922b8cc767fc8a
hansonlam1 <hansonlam1@gmail.com>
2019-06-09 22:20:00 -0700

    minor starting point comments

---
+++ reqHistoricalData.py
@@ -0,0 +1,13 @@
+#very basic attempt to get quote information
+#going to use the minimum needed and enhance further
+#not using threading or ibcontract yet
+
+from ibapi.wrapper import EWrapper
+from ibapi.client import EClient
+from ibapi.contract import Contract as IBContract
+import queue
+
+#need the EClient to call for data
+
+#need a contract
+
