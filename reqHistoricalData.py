0e30903a026d503de3377d51595f27a9c0a852ef
Hanson Lam <45834822+hansonlam1@users.noreply.github.com>
2019-07-13 21:45:14 -0700

    match it up (#9)
    * Dev (#8)
    
    * cleanup
    
    * starting off
    
    * typo
    
    * added contract specs

--- reqHistoricalData.py
+++ reqHistoricalData.py
@@ -1,8 +1,8 @@
 #get a set of historical data from IB
-
 from ibapi.wrapper import EWrapper
 from ibapi.client import EClient
 from threading import Thread
+from ibapi.contract import Contract as IBcontract
 import queue    #queue is a requirement for ibapi python
 
 class IBEWrapper(EWrapper):
@@ -11,6 +11,50 @@ class IBEWrapper(EWrapper):
         histDataQ = queue.Queue() #create a queue to receive info
         self._histDataQ = histDataQ
 
+        return histDataQ
+
+#    override the EWrapper method to put the information coming into a queue
+    def historiclData(self, reqId, bar)
+        #takes the incoming data and put it in a queue
+        #the EClient method call takes it out of the queue
+
+
+class IBClient(EClient):
+
+    def __init__(self,wrapper):
+        EClient.__init__(self,wrapper)  #matches the init of EClient
+
+#    override the method to call the functions in EClient, for example:   
+    def getHistoricalData(self,
+                            contract,
+                            endDateTime,
+                            durationString,
+                            barSizeSetting,
+                            whatToShow,
+                            useRTH,
+                            formatDate,
+                            keepUptoDate,
+                            List)
+        histdata = self.wrapper.init_getHistoricalData()    #queue that has the incoming data
+
+        self.reqHistoricalData(self,
+                                contract,
+                                endDateTime,
+                                durationString,
+                                barSizeSetting,
+                                whatToShow,
+                                useRTH,
+                                formatDate,
+                                keepUptoDate,
+                                List)
+        print("Getting the info")
+        MAX_WAIT_SECONDS = 10
+
+        try:
+            histDataItem = histdata.get(timeout=MAX_WAIT_SECONDS)
+        except queue.Empty:
+            print("Timed Out")
+            histDataItem = "No Data Items"
         return histDataQ
 
 #    override the EWrapper method to put the information coming into a queue
@@ -58,18 +102,24 @@ class IBApp(IBEWrapper, IBClient):
         setattr(self, "_thread", thread)
     
 #if __name__ == '__main__':
-runapp = IBApp("127.0.0.1", 7496, 99)   #connection settings
-runapp.startApi()   #documentation mentions a run function but the class definition only shows startApi()
+app = IBApp("127.0.0.1", 7496, 99)   #connection settings
+app.startApi()   #documentation mentions a run function but the class definition only shows startApi()
+
+#establish which contract we want to get information for
+#consider having prebuilt contracts in a py file to import
+#online examples have a resolve IB contract step but I'll try without for now
+ibcontract = IBcontract()
+ibcontract.SecType = "CASH"
+ibcontract.Symbol = "EUR"
+ibcontract.Exchange = "IDEALPRO"
+ibcontract.Currency = "USD"
+
+app = IBApp("127.0.0.1", 7496, 99)   #connection settings
+app.startApi()   #documentation mentions a run function but the class definition only shows startApi()
 
 #establish which contract we want to get information for
 
-#get the information
-
-
-
-
-
 #print(x)
 
-runapp.Close()
-runapp.disconnect()
+app.Close()
+app.disconnect()
